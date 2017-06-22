039abee12028ae1fa3a6b0c950cd1a610c8c6d19
sliker <qdjalone@126.com>
2017-06-21 18:16:20 -0700

    Add recovery.c

---
+++ cgi-stu/stu/recovery.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+char * headname ="head.html";
+//char * footname ="foot.html";
+
+int cgiMain()
+{
+
+	FILE * fd;
+
+	char sno[32] = "\0";
+	char sta[2]="\0";
+	int status = 0;
+	char ch;
+		fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+		if(!(fd = fopen(headname, "r"))){
+			fprintf(cgiOut, "Cannot open file, %s\n", headname);
+			return -1;
+		}
+		ch = fgetc(fd);
+
+		while(ch != EOF){
+			fprintf(cgiOut, "%c", ch);
+			ch = fgetc(fd);
+		}
+		fclose(fd);
+
+	status = cgiFormString("sno",  sno, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sno error!\n");
+		return 1;
+	}
+	status = cgiFormString("sta",  sta, 2);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+	sprintf(sql, "update student set sta= %d where sno= %d", atoi(sta),atoi(sno));
+
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+	fprintf(cgiOut, "学生数据已恢复！\n");
+	mysql_close(db);
+
+	return 0;
+}
